de43996dd1abb900cd0bc90047b3a38dfd03f31d
JesusDSaavedra <iam.jesusd88@gmail.com>
2024-07-27 21:02:16 -0500

    Translated languages and frameworks

--- src/content/tech-stack.ts
+++ src/content/tech-stack.ts
@@ -22,31 +22,31 @@ export const technologiesAndFrameworks: TechnologyAndFramework[] = [
     name: 'TypeScript',
     logo: '/typescript.webp',
     url: 'https://www.typescriptlang.org/',
-    'description-en': 'JavaScript that scales',
+    'description-en': 'It is a superset of JavaScript, which essentially adds static types and class-based objects.',
     'description-es':
-      'Es un superconjunto tipado de JavaScript que mejora la escalabilidad y mantenibilidad de aplicaciones web.'
+      'Es un superconjunto de JavaScript, que esencialmente añade tipos estáticos y objetos basados en clases. '
   },
   {
     name: 'React',
     logo: '/react.webp',
     url: 'https://reactjs.org/',
-    'description-en': 'A JavaScript library for building user interfaces',
+    'description-en': 'It is a JavaScript library for creating interactive and efficient user interfaces with reusable and declarative components.',
     'description-es':
     'Es una biblioteca JavaScript para crear interfaces de usuario interactivas y eficientes con componentes reutilizables y declarativos.'
   },
   {
     name: 'Vue.js',
     logo: '/vue.webp',
-    url: 'https://reactjs.org/',
-    'description-en': 'A JavaScript library for building user interfaces',
+    url: 'https://vuejs.org/',
+    'description-en': 'It is a Javascript framework that uses "high decoupling", which allows to progressively create user interfaces.',
     'description-es':
-    'Es una biblioteca JavaScript para crear interfaces de usuario interactivas y eficientes con componentes reutilizables y declarativos.'
+    'Es un framework de Javascript que utiliza "alto desacoplamiento", lo que permite crear progresivamente interfaces de usuario.'
   },
   {
     name: 'JavaScript',
     logo: '/javascript.webp',
     url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-    'description-en': 'A programming language that conforms to the ECMAScript specification',
+    'description-en': 'It is a programming language used mainly in web browsers to create interactivity and dynamism in pages.',
     'description-es':
       'Es un lenguaje de programación utilizado principalmente en navegadores web para crear interactividad y dinamismo en las páginas.'
   },
@@ -54,7 +54,7 @@ export const technologiesAndFrameworks: TechnologyAndFramework[] = [
     name: 'Tailwind CSS',
     logo: '/tailwind.webp',
     url: 'https://tailwindcss.com/',
-    'description-en': 'A utility-first CSS framework',
+    'description-en': 'A utility-first CSS framework.',
     'description-es':
       'Es un framework de CSS utilitario que facilita la creación de interfaces web personalizables y responsivas.'
   },
@@ -62,39 +62,39 @@ export const technologiesAndFrameworks: TechnologyAndFramework[] = [
     name: 'Node.js',
     logo: '/node.webp',
     url: 'https://nodejs.org/',
-    'description-en': "A JavaScript runtime built on Chrome's V8 JavaScript engine",
+    'description-en': "It is a JavaScript runtime environment based on the V8 engine, used to build scalable backend applications.",
     'description-es':
       'Es un entorno de ejecución de JavaScript basado en el motor V8, utilizado para construir aplicaciones backend escalables.'
   },
   {
     name: 'Express.js',
     logo: '/express.webp',
-    url: 'https://nodejs.org/',
-    'description-en': "A JavaScript runtime built on Chrome's V8 JavaScript engine",
+    url: 'https://expressjs.com/',
+    'description-en': "It is a web framework for Node.js. It provides a set of tools that facilitate the creation of web applications and APIs.",
     'description-es':
-      'Es un entorno de ejecución de JavaScript basado en el motor V8, utilizado para construir aplicaciones backend escalables.'
+      'Es un framework web para Node.js. Proporciona un conjunto de herramientas que facilitan la creación de aplicaciones web y API.'
   },
   {
     name: 'Sequelize',
     logo: '/sequelize.webp',
-    url: 'https://www.mongodb.com/',
-    'description-en': 'The most popular database for modern apps',
+    url: 'https://sequelize.org/',
+    'description-en': 'It is an ORM for Node.js that facilitates interaction with relational databases. It acts as a bridge between your JavaScript code and the database.',
     'description-es':
-      'Es una base de datos NoSQL que usa documentos JSON para almacenar datos de manera flexible.'
+      'Es un ORM para Node.js que facilita la interacción con bases de datos relacionales. Actúa como un puente entre tu código JavaScript y la base de datos.'
   },
   {
     name: 'Postgres',
     logo: '/postgres.webp',
-    url: 'https://www.mongodb.com/',
-    'description-en': 'The most popular database for modern apps',
+    url: 'https://www.postgresql.org/',
+    'description-en': 'It is a robust database that offers a comprehensive set of features to store and manage large volumes of data efficiently and securely.',
     'description-es':
-      'Es una base de datos NoSQL que usa documentos JSON para almacenar datos de manera flexible.'
+      'Es una base de datos robusta que ofrece un amplio conjunto de funciones para almacenar y gestionar grandes volúmenes de datos de forma eficaz y segura.'
   },
   {
     name: 'MongoDB',
     logo: '/mongodb.webp',
     url: 'https://www.mongodb.com/',
-    'description-en': 'The most popular database for modern apps',
+    'description-en': 'The most popular database for modern apps.',
     'description-es':
       'Es una base de datos NoSQL que usa documentos JSON para almacenar datos de manera flexible.'
   },
@@ -102,7 +102,7 @@ export const technologiesAndFrameworks: TechnologyAndFramework[] = [
     name: 'Docker',
     logo: '/docker.webp',
     url: 'https://www.docker.com/',
-    'description-en': 'Empowering App Development for Developers',
+    'description-en': 'It is a tool that simplifies the creation and management of containers for software applications.',
     'description-es':
       'Es una herramienta que simplifica la creación y gestión de contenedores para aplicaciones software.'
   },
@@ -110,43 +110,43 @@ export const technologiesAndFrameworks: TechnologyAndFramework[] = [
     name: 'Astro',
     logo: '/astro.webp',
     url: 'https://astro.build/',
-    'description-en': 'The fastest way to build modern websites',
-    'description-es': 'Es un marco de trabajo para construir aplicaciones web que combina renderización estática y dinámica eficientemente.'
+    'description-en': 'It is a framework for building web applications that combines static and dynamic rendering efficiently.',
+    'description-es': 'Es un framework de trabajo para construir aplicaciones web que combina renderización estática y dinámica eficientemente.'
   },
   {
     name: 'Sass',
     logo: '/sass.webp',
     url: 'https://sass-lang.com/',
-    'description-en': 'CSS with superpowers',
+    'description-en': 'It is a CSS preprocessor that allows you to write more efficient and maintainable styles using variables and functions.',
     'description-es': 'Es un preprocesador de CSS que permite escribir estilos más eficientes y mantenibles utilizando variables y funciones.'
   },
   {
     name: 'Ant Design',
     logo: '/ant-design.webp',
     url: 'https://sass-lang.com/',
-    'description-en': 'CSS with superpowers',
-    'description-es': 'Es un preprocesador de CSS que permite escribir estilos más eficientes y mantenibles utilizando variables y funciones.'
+    'description-en': 'It is a library of user interface (UI) components, designed to build elegant and efficient user interfaces. ',
+    'description-es': 'Es una biblioteca de componentes de interfaz de usuario (UI), diseñada para construir interfaces de usuario elegantes y eficientes. '
   },
   {
     name: 'Pinia',
     logo: '/pinia.webp',
     url: 'https://sass-lang.com/',
-    'description-en': 'CSS with superpowers',
-    'description-es': 'Es un preprocesador de CSS que permite escribir estilos más eficientes y mantenibles utilizando variables y funciones.'
+    'description-en': 'It is a state management library designed specifically for Vue.js. ',
+    'description-es': 'Es una librería de gestión de estado diseñada específicamente para Vue.js. '
   },
   {
     name: 'Redux',
     logo: '/redux.webp',
     url: 'https://sass-lang.com/',
-    'description-en': 'CSS with superpowers',
-    'description-es': 'Es un preprocesador de CSS que permite escribir estilos más eficientes y mantenibles utilizando variables y funciones.'
+    'description-en': 'A predictable state management library for JavaScript applications.',
+    'description-es': 'Es una biblioteca de gestión de estado predecible para aplicaciones JavaScript.'
   },
   {
     name: 'TanStack Query',
     logo: '/tanstack-query.webp',
     url: 'https://sass-lang.com/',
-    'description-en': 'CSS with superpowers',
-    'description-es': 'Es un preprocesador de CSS que permite escribir estilos más eficientes y mantenibles utilizando variables y funciones.'
+    'description-en': 'It is like an intelligent data manager, interacting with APIs, caching data and keeping it updated efficiently.',
+    'description-es': 'Es como un gestor de datos inteligente, que se encarga de interactuar con las APIs, almacenar los datos en caché y mantenerlos actualizados de forma eficiente.'
   },
 ]
 
