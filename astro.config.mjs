0331815359fe9ffa46d95ea88007026d98862bfb
JesusDSaavedra <iam.jesusd88@gmail.com>
2024-08-09 11:54:00 -0500

    config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-import react from '@astrojs/react'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false
-    }),
-    react()
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react()],
   output: 'server',
   i18n: {
     defaultLocale: 'es',
@@ -18,5 +16,6 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
     }
-  }
-})
+  },
+  adapter: vercel()
+});
